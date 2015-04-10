fafbeb25ae47784f2548b07ff29bd9e888aa1fd1
Ian Brennan <ian.brennan@icloud.com>
2015-04-09 23:23:13 -0600

    unjanked user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,14 @@ class UsersController<ApplicationController
     insta_user = insta_api.token(params[:code])
 
 
-    @user = User.create!(token: insta_user[:access_token], name:insta_user[:user][:username], prof_pic:insta_user[:user][:profile_picture])
-    if @user.
-      session[:user_id]=@user.id
+    @user = User.create(token: insta_user[:access_token], name:insta_user[:user][:username], prof_pic:insta_user[:user][:profile_picture])
+    session[:user_id]=@user.id
+    unless User.all.pluck(:token).include?(@user.token)
+      @user.save
       redirect_to root_path
 
+    else
+      redirect_to root_path
     end
 
   end

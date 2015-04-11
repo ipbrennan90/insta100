4632377d0d84b7bb0e30609906ba4b46a820c8e0
Ian Brennan <ian.brennan@icloud.com>
2015-04-11 17:16:20 -0600

    buggy

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -10,24 +10,26 @@ class LandingController < ApplicationController
 
 
       unless @search_type =="2"
+        binding.pry
         @search=params[:search]
+
       end
 
-      if @search_type
-        if @search_type == "1"
-          @response = @insta_api.tag_photos("#{@search}", "#{current_user.token}")
-        elsif @search_type =="2"
-          @location=Location.create!(address: "#{@search}")
-          @response=@insta_api.location_photos(@location.latitude, @location.longitude, "#{current_user.token}")
-        elsif @search_type =="3"
 
-          @response = @insta_api.user_selfie("#{current_user.client_id}","#{current_user.token}")
-          binding.pry
-        elsif @search_type == "4"
+      if @search_type == "1"
+        binding.pry
+        @response = @insta_api.tag_photos("#{@search}", "#{current_user.token}")
 
-          @response = @insta_api.liked_feed("#{current_user.token}")
+      elsif @search_type =="2"
+        @location=Location.create!(address: "#{@search}")
+        @response=@insta_api.location_photos(@location.latitude, @location.longitude, "#{current_user.token}")
+      elsif @search_type =="3"
 
-        end
+        @response = @insta_api.user_selfie("#{current_user.client_id}","#{current_user.token}")
+      elsif @search_type == "4"
+
+        @response = @insta_api.liked_feed("#{current_user.token}")
+
       end
     else
       token = ENV['API_TOKEN']
